6644787f14aa73e836fdf2997f19c19b985e966d
Mehdi Jafarpour <94742396+Mehdi-Jafarpour@users.noreply.github.com>
2025-06-04 03:06:02 +0400

    fix: strapi plugin cors

--- teromix-cms/config/middlewares.ts
+++ teromix-cms/config/middlewares.ts
@@ -2,11 +2,19 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://teromix.ge'], 
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: '*',
+    },
+  },
+  
 ];
--- teromix-cms/config/plugins.ts
+++ teromix-cms/config/plugins.ts
@@ -1,11 +1,3 @@
 export default {
-  'strapi-plugin-cors': {
-    enabled: true,
-    config: {
-      origin: ['https://teromix.com', 'http://localhost:3000'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      keepHeaderOnError: true,
-    },
-  },
+  
 };
